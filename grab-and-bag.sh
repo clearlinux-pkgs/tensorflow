41e721803f7f96505e787a9c7a1b240d14ff30bf
Arjan van de Ven <arjan@linux.intel.com>
2017-02-11 17:06:35 +0000

    fix avx512 flags to be compatible iwth both skylake and knl

--- grab-and-bag.sh
+++ grab-and-bag.sh
@@ -24,7 +24,7 @@ pushd tensorflow-1.0.0-rc2
 	export TF_BUILD_MAVX=MAVX512
 	./configure < ../answers.txt
 	bazel clean
-	bazel build -c opt --copt=-mavx512 --copt=-O3 --copt=-march=skylake-avx512 --copt=-mfma --copt=-g1  //tensorflow/tools/pip_package:build_pip_package
+	bazel build -c opt --copt=-DEIGEN_VECTORIZE_AVX512 --copt=-mavx2 --copt=-mavx512f --copt=-mavx512cd --copt=-O3 --copt=-march=skylake --copt=-mfma --copt=-g1  //tensorflow/tools/pip_package:build_pip_package
 	bazel-bin/tensorflow/tools/pip_package/build_pip_package $PWD/../tmp/
 	mv ../tmp/tensorflow-1.0.0rc2-cp36-cp36m-linux_x86_64.whl  ../tmp/tensorflow-1.0.0rc2-cp36-cp36m-linux_x86_64.whlavx512
 popd
